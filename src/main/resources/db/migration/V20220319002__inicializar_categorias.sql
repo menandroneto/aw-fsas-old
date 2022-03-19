8e434de08f304ab9684ccfbd8fe431f91e301192
menandro.neto <menandro.neto@gmail.com>
2022-03-19 16:32:57 -0300

    Implantado Flyway
    Adicionado 2 migrações para incluir tabela e inicializar com registros iniciais as categorias de contas monetarias

---
+++ src/main/resources/db/migration/V20220319002__inicializar_categorias.sql
@@ -0,0 +1,5 @@
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
