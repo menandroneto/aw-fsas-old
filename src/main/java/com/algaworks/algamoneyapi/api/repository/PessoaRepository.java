e784f79ca3ce1b035c14b1c3ea002992b5a12f58
menandro.neto <menandro.neto@gmail.com>
2022-03-21 06:21:09 -0300

    Tratamemto para entidade de pessoa

---
+++ src/main/java/com/algaworks/algamoneyapi/api/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.algamoneyapi.api.repository;
+
+import com.algaworks.algamoneyapi.api.model.Pessoa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    
+}
