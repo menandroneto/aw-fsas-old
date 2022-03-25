ba236f05d94f7c4418d0055872e88d7cd5bcb7e2
menandro.neto <menandro.neto@gmail.com>
2022-03-25 03:33:50 -0300

    5.3. Desafio: Cadastrando o primeiro lançamento

--- src/main/java/com/algaworks/algamoneyapi/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoneyapi/api/resource/LancamentoResource.java
@@ -3,13 +3,21 @@ package com.algaworks.algamoneyapi.api.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.algaworks.algamoneyapi.api.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.api.model.Lancamento;
 import com.algaworks.algamoneyapi.api.repository.LancamentoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LancamentoResource {
     @Autowired
     private LancamentoRepository lancamentoRepository;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping
     public List<Lancamento> listar(){
@@ -28,6 +39,13 @@ public class LancamentoResource {
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo){
         Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
         return lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 
 }
